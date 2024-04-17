92b918258e81bb4621ab46e04c059804dd3a760f
MichaelHinrichs <mchinrichs@gmail.com>
2024-04-17 15:16:10 -0500

    Determine file types and give extensions

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ namespace Lycop_Extractor
 {
     class Program
     {
+        static BinaryReader br;
         static void Main(string[] args)
         {
-            BinaryReader br = new BinaryReader(File.OpenRead(args[0]));
+            br = new BinaryReader(File.OpenRead(args[0]));
             br.BaseStream.Position = 20;
 
             int n = 0;
@@ -20,11 +21,39 @@ namespace Lycop_Extractor
                 Array.Reverse(variable);
                 int size = BitConverter.ToInt32(variable, 0);
 
-                BinaryWriter bw = new(File.Create(path + "\\" + n));
+                string type = GetType();
+                BinaryWriter bw = new(File.Create(path + "\\" + n + type));
+
+                if(type == ".zlib")
+                    type -= 4;
 
                 bw.Write(br.ReadBytes(size));
                 bw.Close();
                 n++;
+            }
+        }
+
+        static new string GetType()
+        {
+            byte[] magicBytes = br.ReadBytes(4);
+            br.BaseStream.Position -= 4;
+            string magic = new(System.Text.Encoding.UTF7.GetString(magicBytes));
+            switch (magic)
+            {
+                case "OggS":
+                    return ".ogg";
+                case "\u0089PNG":
+                    return ".png";
+                case "RIFF":
+                    return ".wav";
+                default:
+                    br.BaseStream.Position += 4;
+                    magicBytes = br.ReadBytes(2);
+                    if(magicBytes[0] != 0x78 || magicBytes[1] != 0x9C)
+                        throw new Exception("Unrecognized subfile type.");
+
+                    br.BaseStream.Position -= 2;
+                    return ".zlib";
             }
         }
     }

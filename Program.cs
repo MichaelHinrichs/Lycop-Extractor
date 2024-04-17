f15d8de9e602155032b7cb00f6a651c73dbcd0f1
MichaelHinrichs <mchinrichs@gmail.com>
2024-04-17 14:37:23 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Written for Captain Lycop : Invasion of the Heters. https://store.steampowered.com/app/525070
+using System;
+using System.IO;
+
+namespace Lycop_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryReader br = new BinaryReader(File.OpenRead(args[0]));
+            br.BaseStream.Position = 20;
+
+            int n = 0;
+            string path = Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]);
+            Directory.CreateDirectory(path);   
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                var variable = br.ReadBytes(4);
+                Array.Reverse(variable);
+                int size = BitConverter.ToInt32(variable, 0);
+
+                BinaryWriter bw = new(File.Create(path + "\\" + n));
+
+                bw.Write(br.ReadBytes(size));
+                bw.Close();
+                n++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lycop-Extractor
+ A tool to extract the contents of the file "data.qrc.rcc", from the game [Captain Lycop: Invasion of the Heters](https://store.steampowered.com/app/525070/Captain_Lycop__Invasion_of_the_Heters/).

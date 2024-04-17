3c44a8ab480c39b60f1a4e4b815794e288270fb5
MichaelHinrichs <mchinrichs@gmail.com>
2024-04-17 15:48:18 -0500

    Decompress zlib files

--- Program.cs
+++ Program.cs
@@ -1,36 +1,62 @@
 ﻿//Written for Captain Lycop : Invasion of the Heters. https://store.steampowered.com/app/525070
 using System;
 using System.IO;
+using System.IO.Compression;
 
 namespace Lycop_Extractor
 {
     class Program
     {
         static BinaryReader br;
+        static int size, n;
+        static string path, type;
         static void Main(string[] args)
         {
             br = new BinaryReader(File.OpenRead(args[0]));
             br.BaseStream.Position = 20;
 
-            int n = 0;
-            string path = Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]);
+            n = 0;
+            path = Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]);
             Directory.CreateDirectory(path);   
             while (br.BaseStream.Position < br.BaseStream.Length)
             {
                 var variable = br.ReadBytes(4);
                 Array.Reverse(variable);
-                int size = BitConverter.ToInt32(variable, 0);
+                size = BitConverter.ToInt32(variable, 0);
 
-                string type = GetType();
-                BinaryWriter bw = new(File.Create(path + "\\" + n + type));
+                type = GetType();
+                if (type == ".zlib")
+                {
+                    variable = br.ReadBytes(4);
+                    Array.Reverse(variable);
+                    int sizeUncompressed = BitConverter.ToInt32(variable, 0);
 
-                if(type == ".zlib")
-                    size -= 4;
+                    br.ReadInt16();
+                    MemoryStream ms = new();
+                    using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes(size - 6)), CompressionMode.Decompress))
+                        ds.CopyTo(ms);
 
-                bw.Write(br.ReadBytes(size));
-                bw.Close();
+                    long position = br.BaseStream.Position;
+                    size = sizeUncompressed;
+
+                    br = new(ms);
+                    br.BaseStream.Position = 0;
+                    type = GetType();
+                    WriteFile();
+                    br = new BinaryReader(File.OpenRead(args[0]));
+                    br.BaseStream.Position = position;
+                }
+                else
+                    WriteFile();
                 n++;
             }
+        }
+
+        static void WriteFile()
+        {
+            BinaryWriter bw = new(File.Create(path + "\\" + n + type));
+            bw.Write(br.ReadBytes(size));
+            bw.Close();
         }
 
         static new string GetType()
@@ -52,7 +78,7 @@ namespace Lycop_Extractor
                     if(magicBytes[0] != 0x78 || magicBytes[1] != 0x9C)
                         throw new Exception("Unrecognized subfile type.");
 
-                    br.BaseStream.Position -= 2;
+                    br.BaseStream.Position -= 6;
                     return ".zlib";
             }
         }
